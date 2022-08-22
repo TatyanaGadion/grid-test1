0614ddd168fb3ae54580afbcc3678640e94d2349
TatyanaGadion <taluka1991@gmail.com>
2022-08-22 18:42:10 +0300

    завершение работы над section-about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
-
-// $(document).ready(function () {
-
-//   $('.header-navbar__hamburger').on('click', function () {
-//     $(this).toggleClass('active');
-//     $('.header-navbar__list').toggleClass('active');
-//   });
-
-// });
-
 $(document).ready(function(){
 	$('.header-navbar__btn').click(function(){
 		$(this).toggleClass('header-navbar__btn--active');
